1c99f58684c7040b30826d643a48cd8605a757a3
Alex Picior <alexpicior@yahoo.com>
2024-12-02 21:24:42 +0200

    item10

---
+++ tema3PAOO/inc/MyClass.hpp
@@ -0,0 +1,30 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+#include <iostream>
+#include <cstring>
+
+class MyClass {
+private:
+    char* data;
+    int value;
+
+public:
+    MyClass(const char* initData = "", int initValue = 0)
+        : value(initValue) {
+        data = new char[strlen(initData) + 1];
+        strcpy(data, initData);
+        std::cout << "Constructor apelat.\n";
+    }
+
+    MyClass& operator=(const MyClass& other) {
+        std::cout << "Operator de asignare apelat.\n";
+        return *this;
+    }
+
+    void display() const {
+        std::cout << "Data: " << data << ", Value: " << value << "\n";
+    }
+};
+
+#endif 
